ed29849bb7e96bc91568be8926bc772d2b011129
jeremy <jeremy.nowak@laplateforme.io>
2024-01-24 17:03:24 +0100

    Try to fix issue

--- app/Interface/DatabaseInterface.php
+++ app/Interface/DatabaseInterface.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Interfaces;
+namespace App\Interface;
 
 interface DatabaseInterface{
 
--- app/Model/AbstractDatabase.php
+++ app/Model/AbstractDatabase.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Model;
-use App\Interfaces\DatabaseInterface;
+use App\Interface\DatabaseInterface;
 use PDO;
 use PDOException;
 
