371efbccd9e897fc82659e918fae31ec61fca6a8
jeremy <jeremy.nowak@laplateforme.io>
2024-01-24 16:21:34 +0100

    Interface implementation

---
+++ app/Model/AbstractDatabase.php
@@ -0,0 +1,96 @@
+<?php
+namespace App\Model;
+use App\Interfaces\AbstractDatabaseInterface;
+use PDO;
+use PDOException;
+
+abstract class AbstractDatabase implements AbstractDatabaseInterface{
+    protected $bdd;
+    public $host;
+    public $dbname;
+    public $dbUser;
+    public $dbPass;
+
+    public function __construct() {
+        $this->host = 'localhost';
+        $this->dbname = '';
+        $this->dbUser = 'root';
+        $this->dbPass = '';
+
+        try {
+            $this->bdd = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->dbUser, $this->dbPass);
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->bdd->exec("set names utf8");
+            if ($this->bdd) {
+                echo "Connected to the <strong>$this->dbname</strong> database successfully!";
+            }
+        } catch (PDOException $e) {
+            echo "Error : " . $e->getMessage();
+            die();
+        }
+    }
+
+    public function createNewDbAbstract($db) {
+        try {
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $sql = "CREATE DATABASE IF NOT EXISTS $db";
+            $this->bdd->exec($sql);
+            
+            echo "Database created successfully with the name $db";
+
+            return true;
+
+        } catch (PDOException $e) {
+            echo "Error creating database: " . $e->getMessage();
+
+            return false;
+        }
+    }
+
+
+    public function createNewTable($db, $table) {
+        try {
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $sql = "CREATE TABLE IF NOT EXISTS $db.$table (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                firstname VARCHAR(30) NOT NULL,
+                lastname VARCHAR(30) NOT NULL,
+                email VARCHAR(50),
+                reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                )";
+            $this->bdd->exec($sql);
+            
+            echo "Table $table created successfully in $db database";
+
+            return true;
+
+        } catch (PDOException $e) {
+            echo "Error creating table: " . $e->getMessage();
+
+            return false;
+        }
+    }
+
+    public function dropTable($db, $table) {
+        try {
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $sql = "DROP TABLE $db.$table";
+            $this->bdd->exec($sql);
+            
+            echo "Table $table dropped successfully in $db database";
+
+            return true;
+
+        } catch (PDOException $e) {
+            echo "Error dropping table: " . $e->getMessage();
+
+            return false;
+        }
+    }
+
+
+}
+?>
--- app/Model/DataBaseCreation.php
+++ app/Model/DataBaseCreation.php
@@ -2,19 +2,14 @@
 namespace App\Model;
 use PDO;
 use PDOException;
-use App\Model\Database;
+use App\Model\AbstractDatabase;
 
-class DataBaseCreation extends Database {
+class DataBaseCreation extends AbstractDatabase {
 
-    private string $connServername;
-    private string $connUsername;
-    private string $connPassword;
 
-    public function __construct($connServername = null, $connUsername = null, $connPassword = null) {
-        
-        $this->connServername = $connServername;
-        $this->connUsername = $connUsername;
-        $this->connPassword = $connPassword;
+
+    public function __construct() {
+
         parent::__construct();
     } 
     
@@ -22,6 +17,26 @@ class DataBaseCreation extends Database {
 
         return $this->createNewDbAbstract($db);
 
+    }
+
+
+    public function createNewDbAbstract($db){
+
+    }
+    public function createNewTableAbstract($db, $table){
+
+    }
+    public function getAll(){
+
+    }
+    public function getOne($id){
+
+    }
+    public function updateTableAbstract($db, $table){
+
+    }
+    public function dropTableAbstract($db, $table){
+        
     }
 }
 ?>
