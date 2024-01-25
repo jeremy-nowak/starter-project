f16b64db8cd177ee89bd8bca58c2e57433661778
jeremy <jeremy.nowak@laplateforme.io>
2024-01-25 08:20:21 +0100

    Resolved issue #1; Restructured project with new 'Database' namespace

--- app/Database/DataBaseCreation.php
+++ app/Database/DataBaseCreation.php
@@ -1,32 +1,38 @@
 <?php
-namespace App\Model;
+namespace App\Database;
 use PDO;
 use PDOException;
-use App\Model\AbstractDatabase;
-class DataBaseUpdate extends AbstractDatabase{
+use App\Database\AbstractDatabase;
+
+class DataBaseCreation extends AbstractDatabase {
 
 
-    public function createNewDbAbstract($db){
-        
+
+    public function __construct() {
+
+        parent::__construct();
+    } 
+    
+    public function createNewDb($db) {
+
+        return $this->createNewDbAbstract($db);
+
     }
+
     public function createNewTableAbstract($db, $table){
-        
+
     }
     public function getAll(){
-        
+
     }
     public function getOne($id){
-        
+
     }
     public function updateTableAbstract($db, $table){
-        
+
     }
     public function dropTableAbstract($db, $table){
         
     }
-
 }
-
-
-
 ?>
--- app/Database/DataBaseUpdate.php
+++ app/Database/DataBaseUpdate.php
@@ -1,38 +1,32 @@
 <?php
-namespace App\Model;
+namespace App\Database;
 use PDO;
 use PDOException;
-use App\Model\AbstractDatabase;
-
-class DataBaseCreation extends AbstractDatabase {
-
+use App\Database\AbstractDatabase;
+class DataBaseUpdate extends AbstractDatabase{
 
 
-    public function __construct() {
-
-        parent::__construct();
-    } 
-    
-    public function createNewDb($db) {
-
-        return $this->createNewDbAbstract($db);
-
+    public function createNewDbAbstract($db){
+        
     }
-
     public function createNewTableAbstract($db, $table){
-
+        
     }
     public function getAll(){
-
+        
     }
     public function getOne($id){
-
+        
     }
     public function updateTableAbstract($db, $table){
-
+        
     }
     public function dropTableAbstract($db, $table){
         
     }
+
 }
+
+
+
 ?>
